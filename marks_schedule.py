ca7f6e414310fb96433346c717e2898c358c2cdf
princessradebe <princessradebe9819@gmail.com>
2024-09-09 18:43:36 +0200

    Create marks_schedule.py

---
+++ marks_schedule.py
@@ -0,0 +1,16 @@
+# Get stu_num from user
+stu_num = int(input('Enter student number: '))
+
+# Get test scores from the user
+print('Enter your test scores')
+test_1 = input('Test 1: ')
+test_2 = input('Test 2: ')
+test_3 = input('Test 3: ')
+test_4 = input('Test 4: ')
+
+# Calculate the final grade
+final_grade = (test_1 + test_2 + test_3 + test_4)/4
+
+# Display the student number and their grades
+print('Student No: ', stu_num)
+print("Final grade: ", final_grade)
